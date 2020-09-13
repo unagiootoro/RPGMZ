0b776ba753e1e34887893457f681b329b88d65c5
unagiootoro <ootoro838861@outlook.jp>
2020-09-13 14:28:40 +0900

    Add english help

--- SkillTree_ClassEx.js
+++ SkillTree_ClassEx.js
@@ -1,5 +1,47 @@
 /*:
 @target MV MZ
+@plugindesc Skill Tree Class Expansion v1.0.1
+@author unagi Otoro
+@url https://raw.githubusercontent.com/unagiootoro/RPGMZ/master/SkillTree_ClassEx.js
+
+@help
+You will be able to have a skill tree for each occupation.
+This allows you to build different skill trees for different professions.
+
+[how to use]
+To build a skill tree for each occupation, enter classId in the type setting.
+For example, if you have the following type settings:
+{
+    actorId: 1,
+    types: [
+        ["Sword technique", "Sword technique", "Get sword technique."],
+        ["Martial Arts", "Martial Arts", "Get Martial Arts."],
+    ]
+}
+
+If you want to apply this setting to Class ID2 instead of Actor ID1 using the Class Extension Plugin, set as follows:
+{
+    classId: 2,
+    types: [
+        ["Sword technique", "Sword technique", "Get sword technique."],
+        ["Martial Arts", "Martial Arts", "Get Martial Arts."],
+    ]
+}
+
+[Specification]
+・When you change jobs, the skills you acquired in your previous profession will remain acquired.
+・By using a common type type, you can create a skill tree type that is shared among multiple classses.
+・If you reset the skill, only the skill tree of the class with the current actor will be reset.
+
+[Important point]
+When using with "SkillTree_IconEx.js", please put this plugin under "SkillTree_IconEx.js".
+
+[License]
+This plugin is available under the terms of the MIT license.
+*/
+
+/*:ja
+@target MV MZ
 @plugindesc スキルツリー 職業拡張 v1.0.0
 @author うなぎおおとろ
 @url https://raw.githubusercontent.com/unagiootoro/RPGMZ/master/SkillTree_ClassEx.js
