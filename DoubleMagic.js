be175d7138138f2336ce5b4c81c861f09d5fade8
unagiootoro <ootoro838861@outlook.jp>
2020-12-08 20:26:28 +0900

    Update to version v1.2.0

--- DoubleMagic.js
+++ DoubleMagic.js
@@ -1,7 +1,7 @@
 /*:
 @target MV MZ
-@plugindesc 連続魔法
-@author うなぎおおとろ(twitter https://twitter.com/unagiootoro8388)
+@plugindesc 連続魔法 v1.2.0
+@author うなぎおおとろ
 
 @param MagicSkillTypeId
 @type number
@@ -22,115 +22,128 @@ trueを指定すると、2回目の魔法発動時のメッセージを変更し
 2回目の魔法発動時のメッセージを指定します。
 
 @help
-一回の行動で連続で魔法を使用できるようになるステートを作成可能にするプラグインです。
-FFの連続魔みたいなのを作ることができます。
-また、「ステート付与装備」プラグインと併用すれば、ドラクエの山彦の帽子みたいなものを作ることもできます。
+一回の行動で連続で魔法を使用できるようになるステート/武器/防具/スキルを作成可能にするプラグインです。
+ドラクエの山彦の帽子みたいなのを作ることができます。
 
 [使い方]
-連続魔法を可能にしたいステートのメモ欄に
+連続魔法を可能にしたいステート/武器/防具/スキルのメモ欄に
 <DoubleMagic>
 と記述してください。
 
+<DoubleMagic>を記載したのが、ステート/武器/防具/スキルのいずれかによって
+連続魔法が使用可能になるタイミングが異なります。
+ステートに記載した場合は、当該ステートになっている間、連続魔法が可能になります。
+武器/防具に記載した場合は、当該武器/防具を装備している間、連続魔法が可能になります。
+スキルに記載した場合、当該スキルを習得していれば連続魔法が可能になります。
+
 [ライセンス]
 このプラグインは、MITライセンスの条件の下で利用可能です。
-
-[更新履歴]
-v1.1.0 プラグインパラメータを変更
-v1.0.0 新規作成
 */
-{
-    "use strict";
 
-    const pluginName = document.currentScript.src.match(/.+\/(.+)\.js/)[1];
-    const params = PluginManager.parameters(pluginName);
-    const MagicSkillTypeId = parseInt(params["MagicSkillTypeId"]);
-    const EnableUseConstnuousMagicMessage = (params["EnableUseConstnuousMagicMessage"] === "true" ? true : false);
-    const UseConstnuousMagicMessage = params["UseConstnuousMagicMessage"];
+const DoubleMagicPluginName = document.currentScript.src.match(/.+\/(.+)\.js/)[1];
 
-    /* class Game_Battler */
-    const _Game_Battler_removeCurrentAction = Game_Battler.prototype.removeCurrentAction
-    Game_Battler.prototype.removeCurrentAction = function() {
-        const action = this.currentAction();
-        action.reduceContinuousActionCount();
-        if (action.continuousActionCount() === 0) {
-            _Game_Battler_removeCurrentAction.call(this);
-        }
-    };
+(() => {
+"use strict";
 
+const params = PluginManager.parameters(DoubleMagicPluginName);
+const MagicSkillTypeId = parseInt(params["MagicSkillTypeId"]);
+const EnableUseConstnuousMagicMessage = (params["EnableUseConstnuousMagicMessage"] === "true" ? true : false);
+const UseConstnuousMagicMessage = params["UseConstnuousMagicMessage"];
 
-    /* class Game_Actor */
-    Game_Actor.prototype.isContinuousMagic = function() {
-        for (let stateId of this._states) {
-            if ($dataStates[stateId].meta.DoubleMagic) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-
-    /* class Game_Action */
-    const _Game_Action_initialize = Game_Action.prototype.initialize;
-    Game_Action.prototype.initialize = function(subject, forcing) {
-        _Game_Action_initialize.call(this, subject, forcing);
-        this._continuousActionCount = 1;
-        this._maxContinuousActionCount = 1;
-    };
-
-    const _Game_Action_setSkill = Game_Action.prototype.setSkill;
-    Game_Action.prototype.setSkill = function(skillId) {
-        _Game_Action_setSkill.call(this, skillId);
-        if (this.subject() instanceof Game_Actor && this.subject().isContinuousMagic()) {
-            if (this.item().stypeId === MagicSkillTypeId) {
-                this._continuousActionCount = 2;
-                this._maxContinuousActionCount = 2;
-            }
-        }
-    };
-
-    Game_Action.prototype.continuousActionCount = function() {
-        return this._continuousActionCount;
-    };
-
-    Game_Action.prototype.reduceContinuousActionCount = function() {
-        this._continuousActionCount -= 1;
-    };
-
-    Game_Action.prototype.isFirstAction = function() {
-        return this._maxContinuousActionCount === this._continuousActionCount;
+/* class Game_Battler */
+const _Game_Battler_removeCurrentAction = Game_Battler.prototype.removeCurrentAction
+Game_Battler.prototype.removeCurrentAction = function() {
+    const action = this.currentAction();
+    action.reduceContinuousActionCount();
+    if (action.continuousActionCount() === 0) {
+        _Game_Battler_removeCurrentAction.call(this);
     }
-
-
-    /* class Window_BattleLog */
-    Window_BattleLog.prototype.startAction = function(subject, action, targets) {
-        const item = action.item();
-        if (action.isFirstAction()) {
-            this.push("performActionStart", subject, action);
-            this.push("waitForMovement");
-        }
-        this.push("performAction", subject, action);   
-        this.push("showAnimation", subject, targets.clone(), item.animationId);
-        if (action.isFirstAction() || !EnableUseConstnuousMagicMessage) {
-            this.displayAction(subject, item);
-        } else {
-            this.displayConstinuousAction(subject, item);
-        }
-    };
-
-    Window_BattleLog.prototype.displayConstinuousAction = function(subject, item) {
-        const numMethods = this._methods.length;
-        this.push("addText", UseConstnuousMagicMessage);
-        if (this._methods.length === numMethods) {
-            this.push("wait");
-        }
-    };
-
-    Window_BattleLog.prototype.endAction = function(subject) {
-        const action = subject.currentAction();
-        this.push("waitForNewLine");
-        this.push("clear");
-        if (!action) {
-            this.push("performActionEnd", subject);
-        }
-    };
 };
+
+Game_Battler.prototype.isContinuousMagic = function() {
+    for (const stateId of this._states) {
+        if ($dataStates[stateId].meta.DoubleMagic) return true;
+    }
+    return false;
+};
+
+
+/* class Game_Actor */
+Game_Actor.prototype.isContinuousMagic = function() {
+    const result = Game_Battler.prototype.isContinuousMagic.call(this);
+    if (result) return true;
+    for (const equip of this._states) {
+        if (equip.meta.DoubleMagic) return true;
+    }
+    for (const skillId of this._skills) {
+        if ($dataSkills[skillId].meta.DoubleMagic) return true;
+    }
+    return false;
+};
+
+
+/* class Game_Action */
+const _Game_Action_initialize = Game_Action.prototype.initialize;
+Game_Action.prototype.initialize = function(subject, forcing) {
+    _Game_Action_initialize.call(this, subject, forcing);
+    this._continuousActionCount = 1;
+    this._maxContinuousActionCount = 1;
+};
+
+const _Game_Action_setSkill = Game_Action.prototype.setSkill;
+Game_Action.prototype.setSkill = function(skillId) {
+    _Game_Action_setSkill.call(this, skillId);
+    if (this.subject().isContinuousMagic()) {
+        if (this.item().stypeId === MagicSkillTypeId) {
+            this._continuousActionCount = 2;
+            this._maxContinuousActionCount = 2;
+        }
+    }
+};
+
+Game_Action.prototype.continuousActionCount = function() {
+    return this._continuousActionCount;
+};
+
+Game_Action.prototype.reduceContinuousActionCount = function() {
+    this._continuousActionCount -= 1;
+};
+
+Game_Action.prototype.isFirstAction = function() {
+    return this._maxContinuousActionCount === this._continuousActionCount;
+}
+
+
+/* class Window_BattleLog */
+Window_BattleLog.prototype.startAction = function(subject, action, targets) {
+    const item = action.item();
+    if (action.isFirstAction()) {
+        this.push("performActionStart", subject, action);
+        this.push("waitForMovement");
+    }
+    this.push("performAction", subject, action);   
+    this.push("showAnimation", subject, targets.clone(), item.animationId);
+    if (action.isFirstAction() || !EnableUseConstnuousMagicMessage) {
+        this.displayAction(subject, item);
+    } else {
+        this.displayConstinuousAction(subject, item);
+    }
+};
+
+Window_BattleLog.prototype.displayConstinuousAction = function(subject, item) {
+    const numMethods = this._methods.length;
+    this.push('addText', UseConstnuousMagicMessage);
+    if (this._methods.length === numMethods) {
+        this.push('wait');
+    }
+};
+
+Window_BattleLog.prototype.endAction = function(subject) {
+    const action = subject.currentAction();
+    this.push("waitForNewLine");
+    this.push("clear");
+    if (!action) {
+        this.push("performActionEnd", subject);
+    }
+};
+})();
