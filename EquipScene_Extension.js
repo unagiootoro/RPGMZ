402d4de12a8577eeace3d29f2f3de7c76243c2a9
unagiootoro <ootoro838861@outlook.jp>
2021-02-13 10:20:09 +0900

    Update to version v1.4.0

--- EquipScene_Extension.js
+++ EquipScene_Extension.js
@@ -1,76 +1,194 @@
 /*:
 @target MZ
-@plugindesc 装備シーン拡張 v1.3.0
-@author うなぎおおとろ
+@plugindesc Equipment scene expansion v1.4.0
+@author unagi ootoro
 @url https://raw.githubusercontent.com/unagiootoro/RPGMZ/master/EquipScene_Extension.js
+
+@help
+It is a plug-in that expands the equipment scene.
+By installing this plug-in, you will be able to change the layout of the equipment screen and expand the equipment slot.
+
+【Layout change】
+By setting LayoutMode, you can change the layout so that equipment slots and equipment items are displayed at the same time.
+If LayoutMode is set to 1, the status screen remains the same and equipment items are displayed in one line.
+If LayoutMode is set to 2, the status screen will be reduced and equipment items will be displayed in 2 lines.
+
+
+【Equipment slot expansion】
+This function assigns an equipment type to each equipment slot.
+By using this function, you will be able to equip multiple ornaments and multiple weapons of different systems.
+
+■ Equipment slot settings
+Set the equipment slot by setting the plug-in parameter "Default Equip Slots".
+
+■ Setting equipment slots for individual characters
+In the memo field of either actor / occupation / state
+<EquipSlots: Equipment Slots>
+By writing, you can set individual equipment slots for each character.
+Set the equipment slot in the same format as the plug-in parameter "Default Equip Slots".
+(Setting Example)
+<EquipSlots: [1, 2, 3, 4, 5]>
+
+■ Multiple types of weapons
+You can set multiple weapon types with the plug-in parameter "Weapon Types".
+This allows you to separate weapons by lineage, just as armor separates into body, head, and shield.
+(Example of setting the plug-in parameter "Weapon Types")
+[1, 2]
+
+Next, give the weapon a weapon type.
+In the memo field of the weapon
+<WeaponType: Weapon Type ID>
+It is described in the format. If you do not specify this setting, the weapon type ID will be 1.
+(Weapon type ID setting example)
+<WeaponType: 2>
+
+■ Prohibit the equipment of the same type of weapon / armor
+This function is used when you want to prevent dual wield characters from equipping multiple two-handed swords.
+In the plugin parameter "NotMultiEquipWeapon" or "NotMultiEquipArmor"
+If you list the weapon / armor type ID that prohibits multiple equipment,
+Weapons / armor with the corresponding type ID cannot be equipped more than once.
+
+Also, by making the following settings, you will be able to set equipment coexistence prohibition for each weapon / armor.
+In the memo field of the weapon
+<NotMultiEquipWeaponIds: [Weapon ID1, Weapon ID2, ...]>
+Or in the memo field of the armor
+<NotMultiEquipArmorIds: [Armor ID1, Armor ID2, ...]>
+You can set the equipment coexistence prohibition by describing.
+
+For example, when setting the coexistence prohibition of weapon ID1 and weapon ID2,
+In the memo field of weapon ID1
+<NotMultiEquipWeaponIds: [2]>
+In the memo field of weapon ID2
+<NotMultiEquipWeaponIds: [1]>
+It is described as. Be careful not to set only one of the equipment coexistence prohibition settings.
+
+■ Set only one weapon / armor that can be equipped
+In the memo field of the weapon or armor
+<UniqueEquip>
+By writing, you can set only one weapon / armor that can be equipped.
+
+■ Change of initial equipment
+If you change the equipment slot from the default one, the contents of the initial equipment will not be reflected.
+In that case, change the equipment by performing "Forced change" with the plug-in command "Change equipment slot".
+
+
+【License】
+This plugin is available under the terms of the MIT license.
 
 @param LayoutMode
 @type number
 @default 2
-@desc 0: デフォルト  1: 装備スロットと装備アイテムを同時表示  2: 装備アイテムを2行で表示
+@desc 0: Default 1: Show equipment slots and equipment items at the same time 2: Show equipment items in 2 lines
 
 @param ActorFaceHeight
 @type number
 @default 43
-@desc LayoutMode = 2のときのアクターの顔グラフィックの高さを指定します。
+Specifies the height of the actor's face graphic when @desc LayoutMode = 2.
 
 @param DefaultEquipSlots
 @type string
-@default [1, 2, 3, 4, 5]
-@desc デフォルトの装備スロットです。スロット順に装備タイプIDを指定します。
+@default ["1","2","3","4","5"]
+@desc This is the default equipment slot. Specify the equipment type ID in slot order.
 
 @param WeaponTypes
 @type string
-@default [1]
-@desc 武器の装備タイプIDの一覧を指定します。
+@default ["1"]
+@desc Specifies a list of weapon equipment type IDs.
 
 @param DualWieldSlot
 @type number
 @default 1
-@desc 二刀流の場合に書き換えるスロットのインデックスを指定します。(例)2番目のスロットを書き換える場合は1を指定
+@desc Specifies the index of the slot to be rewritten in the case of dual wield. (Example) Specify 1 to rewrite the second slot
 
 @param WeaponTypeWhenDualWield
 @type number
 @default 1
-@desc 二刀流時に書き換える装備タイプIDを指定します。
+@desc Specifies the equipment type ID to be rewritten during dual wield.
 
 @param NotMultiEquipWeapon
 @type number[]
 @default []
-@desc 複数装備を禁止する武器タイプIDを指定します。
+@desc Specifies the weapon type ID that prohibits multiple equipment.
 
 @param NotMultiEquipArmor
 @type number[]
 @default []
-@desc 複数装備を禁止する防具タイプIDを指定します。
+@desc Specifies the armor type ID that prohibits multiple equipment.
 
 @param RemoveEquipText
 @type string
-@default 外す
-@desc 武器を外すときの空枠に表示する文言を指定します。
+@default remove
+@desc Specifies the wording to be displayed in the empty frame when removing the weapon.
 
 @param RemoveEquipIconIndex
 @type number
 @default 0
-@desc 武器を外すときの空枠に表示するアイコンを指定します。
+@desc Specifies the icon to display in the empty frame when removing the weapon.
 
 @param DisableCommandWindow
 @type boolean
 @default false
-@desc trueを設定すると最強装備等を表示するウィンドウを非表示にします。
+When @desc true is set, the window that displays the strongest equipment etc. is hidden.
+
+
+@command ChangeEquipSlot
+@text Equipment slot change
+@desc
+Changes the equipment slot of the specified actor.
+
+@arg ActorId
+@text Actor ID
+@type actor
+@default 1
+@desc
+Specify the actor whose equipment slot is to be changed.
+
+@arg SlotIndex
+@text slot index
+@type number
+@default 0
+@desc
+Specifies the index of the equipment slot to change. (Sequential number from 0)
+
+@arg WeaponId
+@text Weapon ID
+@type weapon
+@default 0
+@desc
+Specify the weapon ID to change. If you want to change the armor, specify 0.
+
+@arg ArmorId
+@text Armor ID
+@type armor
+@default 0
+@desc
+Specify the armor ID to change. If you want to change the weapon, specify 0.
+
+@arg ForceChange
+@text forced change
+@type boolean
+@default false
+@desc
+If set to true, the equipment will be forcibly changed.
+*/
+
+/*:ja
+@target MZ
+@plugindesc 装備シーン拡張 v1.4.0
+@author うなぎおおとろ
+@url https://raw.githubusercontent.com/unagiootoro/RPGMZ/master/EquipScene_Extension.js
 
 @help
 装備シーンを拡張するプラグインです。
 このプラグインを導入することで、装備画面のレイアウト変更と装備スロットの拡張ができるようになります。
 
-
-[レイアウト変更]
+【レイアウト変更】
 LayoutModeを設定することで、装備スロットと装備アイテムを同時表示するようにレイアウトを変更します。
 LayoutModeに1を設定した場合は、ステータス画面はそのままで、装備アイテムは1行で表示します。
 LayoutModeに2を設定した場合は、ステータス画面を縮小し、装備アイテムを2行で表示します。
 
 
-[装備スロット拡張]
+【装備スロット拡張】
 装備スロットごとに装備タイプを割り当てる機能です。
 この機能を使用することで、装飾品を複数装備したり、系統の異なる武器を複数装備したりすることができるようになります。
 
@@ -123,8 +241,109 @@ LayoutModeに2を設定した場合は、ステータス画面を縮小し、装
 <UniqueEquip>
 と記載することで、一つだけ装備可能な武器/防具を設定することができます。
 
-[ライセンス]
+■初期装備の変更
+装備スロットをデフォルトのものから変更した場合、初期装備の内容が反映されなくなります。
+その場合、プラグインコマンド「装備スロット変更」で「強制変更」を行うことで装備を変更してください。
+
+
+【ライセンス】
 このプラグインは、MITライセンスの条件の下で利用可能です。
+
+@param LayoutMode
+@type number
+@default 2
+@desc 0: デフォルト  1: 装備スロットと装備アイテムを同時表示  2: 装備アイテムを2行で表示
+
+@param ActorFaceHeight
+@type number
+@default 43
+@desc LayoutMode = 2のときのアクターの顔グラフィックの高さを指定します。
+
+@param DefaultEquipSlots
+@type number[]
+@default ["1","2","3","4","5"]
+@desc デフォルトの装備スロットです。スロット順に装備タイプIDを指定します。
+
+@param WeaponTypes
+@type number[]
+@default ["1"]
+@desc 武器の装備タイプIDの一覧を指定します。
+
+@param DualWieldSlot
+@type number
+@default 1
+@desc 二刀流の場合に書き換えるスロットのインデックスを指定します。(例)2番目のスロットを書き換える場合は1を指定
+
+@param WeaponTypeWhenDualWield
+@type number
+@default 1
+@desc 二刀流時に書き換える装備タイプIDを指定します。
+
+@param NotMultiEquipWeapon
+@type number[]
+@default []
+@desc 複数装備を禁止する武器タイプIDを指定します。
+
+@param NotMultiEquipArmor
+@type number[]
+@default []
+@desc 複数装備を禁止する防具タイプIDを指定します。
+
+@param RemoveEquipText
+@type string
+@default 外す
+@desc 武器を外すときの空枠に表示する文言を指定します。
+
+@param RemoveEquipIconIndex
+@type number
+@default 0
+@desc 武器を外すときの空枠に表示するアイコンを指定します。
+
+@param DisableCommandWindow
+@type boolean
+@default false
+@desc trueを設定すると最強装備等を表示するウィンドウを非表示にします。
+
+
+@command ChangeEquipSlot
+@text 装備スロット変更
+@desc
+指定したアクターの装備スロットを変更します。
+
+@arg ActorId
+@text アクターID
+@type actor
+@default 1
+@desc
+装備スロットを変更するアクターを指定します。
+
+@arg SlotIndex
+@text スロットインデックス
+@type number
+@default 0
+@desc
+変更する装備スロットのインデックスを指定します。(0からの連番)
+
+@arg WeaponId
+@text 武器ID
+@type weapon
+@default 0
+@desc
+変更する武器IDを指定します。防具を変更する場合、0を指定してください。
+
+@arg ArmorId
+@text 防具ID
+@type armor
+@default 0
+@desc
+変更する防具IDを指定します。武器を変更する場合、0を指定してください。
+
+@arg ForceChange
+@text 強制変更
+@type boolean
+@default false
+@desc
+trueを設定すると強制的に装備を変更します。
 */
 
 const EquipScenePluginName = document.currentScript.src.match(/.+\/(.+)\.js/)[1];
@@ -132,39 +351,121 @@ const EquipScenePluginName = document.currentScript.src.match(/.+\/(.+)\.js/)[1]
 (() => {
 "use strict";
 
-const parseNumberArray = (params, paramName) => {
-    const errorMessage = `プラグインパラメータ「${paramName}」の読み込みに失敗しました。`
-    try {
-        const param = params[paramName]
-        const slots = eval(param);
-        for (const i of slots) {
-            if (typeof i !== "number") {
-                if (typeof i === "string") {
-                    slots[i] = parseInt(i);
-                } else {
-                    throw new Error();
-                }
+class PluginParamsParser {
+    static parse(params, typeData, predictEnable = true) {
+        return new PluginParamsParser(predictEnable).parse(params, typeData);
+    }
+
+    constructor(predictEnable = true) {
+        this._predictEnable = predictEnable;
+    }
+
+    parse(params, typeData, loopCount = 0) {
+        if (++loopCount > 255) throw new Error("endless loop error");
+        const result = {};
+        for (const name in typeData) {
+            result[name] = this.convertParam(params[name], typeData[name], loopCount);
+        }
+        if (!this._predictEnable) return result;
+        if (typeof params === "object" && !(params instanceof Array)) {
+            for (const name in params) {
+                if (result[name]) continue;
+                const param = params[name];
+                const type = this.predict(param);
+                result[name] = this.convertParam(param, type, loopCount);
             }
         }
-        return slots;
-    } catch(e) {
-        console.log(`ERROR: ${e}`);
-        throw new Error(errorMessage);
+        return result;
     }
-};
 
-const params = PluginManager.parameters(EquipScenePluginName);
-const LayoutMode = parseInt(params["LayoutMode"]);
-const ActorFaceHeight = parseInt(params["ActorFaceHeight"]);
-const DualWieldSlot = parseInt(params["DualWieldSlot"]);
-const DefaultEquipSlots = parseNumberArray(params, "DefaultEquipSlots");
-const WeaponTypes = parseNumberArray(params, "WeaponTypes");
-const WeaponTypeWhenDualWield = parseInt(params["WeaponTypeWhenDualWield"]);
-const NotMultiEquipWeapon = parseNumberArray(params, "NotMultiEquipWeapon");
-const NotMultiEquipArmor = parseNumberArray(params, "NotMultiEquipArmor");
-const RemoveEquipText = params["RemoveEquipText"];
-const RemoveEquipIconIndex = parseInt(params["RemoveEquipIconIndex"]);
-const DisableCommandWindow = params["DisableCommandWindow"] === "true" ? true : false;
+    convertParam(param, type, loopCount) {
+        if (typeof type === "string") {
+            return this.cast(param, type);
+        } else if (typeof type === "object" && type instanceof Array) {
+            const aryParam = JSON.parse(param);
+            if (typeof type[0] === "string") {
+                return aryParam.map(strParam => this.cast(strParam, type[0]));
+            } else {
+                return aryParam.map(strParam => this.parse(JSON.parse(strParam), type[0]), loopCount);
+            }
+        } else if (typeof type === "object") {
+            return this.parse(JSON.parse(param), type, loopCount);
+        } else {
+            throw new Error(`${type} is not string or object`);
+        }
+    }
+
+    cast(param, type) {
+        switch(type) {
+        case "any":
+            if (!this._predictEnable) throw new Error("Predict mode is disable");
+            return this.cast(param, this.predict(param));
+        case "string":
+            return param;
+        case "number":
+            if (param.match(/\d+\.\d+/)) return parseFloat(param);
+            return parseInt(param);
+        case "boolean":
+            return param === "true";
+        default:
+            throw new Error(`Unknow type: ${type}`);
+        }
+    }
+
+    predict(param) {
+        if (param.match(/^\d+$/) || param.match(/^\d+\.\d+$/)) {
+            return "number";
+        } else if (param === "true" || param === "false") {
+            return "boolean";
+        } else {
+            return "string";
+        }
+    }
+}
+
+const typeData = {
+    DefaultEquipSlots: ["any"],
+    WeaponTypes: ["any"],
+    NotMultiEquipWeapon: ["any"],
+    NotMultiEquipArmor: ["any"],
+};
+const PP = PluginParamsParser.parse(PluginManager.parameters(EquipScenePluginName), typeData);
+
+const LayoutMode = PP.LayoutMode;
+const ActorFaceHeight = PP.ActorFaceHeight;
+const DualWieldSlot = PP.DualWieldSlot;
+const DefaultEquipSlots = PP.DefaultEquipSlots;
+const WeaponTypes = PP.WeaponTypes;
+const WeaponTypeWhenDualWield = PP.WeaponTypeWhenDualWield;
+const NotMultiEquipWeapon = PP.NotMultiEquipWeapon;
+const NotMultiEquipArmor = PP.NotMultiEquipArmor;
+const RemoveEquipText = PP.RemoveEquipText;
+const RemoveEquipIconIndex = PP.RemoveEquipIconIndex;
+const DisableCommandWindow = PP.DisableCommandWindow;
+
+// Register plugin commands.
+PluginManager.registerCommand(EquipScenePluginName, "ChangeEquipSlot", function(args) {
+    const params = PluginParamsParser.parse(args, {});
+    const actorId = params.ActorId;
+    const slotIndex = params.SlotIndex
+    const weaponId = params.WeaponId;
+    const armorId = params.ArmorId;
+    const forceChange = params.ForceChange;
+    const actor = $gameActors.actor(actorId);
+    let item = null;
+    if (weaponId > 0) {
+        item = $dataWeapons[weaponId];
+    } else if (armorId > 0) {
+        item = $dataArmors[armorId];
+    }
+    if (!item) return;
+    if (forceChange) {
+        actor.forceChangeEquip(slotIndex, item);
+    } else {
+        actor.changeEquip(slotIndex, item);
+    }
+});
+
 
 // Change equip layout.
 Scene_Equip.prototype.createCommandWindow = function() {
