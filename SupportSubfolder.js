c838cc90f0ea932664707497188419c778766e71
unagiootoro <ootoro838861@outlook.jp>
2021-06-23 02:30:02 +0900

    Bug fix

--- SupportSubfolder.js
+++ SupportSubfolder.js
@@ -1,10 +1,11 @@
 /*:
 @target MZ
-@plugindesc SupportSubfolder
+@plugindesc SupportSubfolder v1.0.0
 @author うなぎおおとろ
 @url https://raw.githubusercontent.com/unagiootoro/RPGMZ/master/SupportSubfolder.js
 @help
-サブフォルダーに対応していないプラグインもサブフォルダーに対応できるようにするプラグインです。
+RPGツクールMZ v1.3.0以降で追加されたサブフォルダー機能に対応していないプラグインも
+サブフォルダーに対応できるようにするプラグインです。
 
 【使い方】
 SupportSubfolder.jsをどのプラグインよりも一番上に導入してください。
@@ -20,7 +21,7 @@ const _PluginManager_parameters = PluginManager.parameters;
 PluginManager.parameters = function(name) {
     const lowerPluginName = name.toLowerCase();
     if (this._parameters[lowerPluginName] == null) {
-        const reg = new RegExp(lowerPluginName);
+        const reg = new RegExp(lowerPluginName + "$");
         const pluginPath = Object.keys(this._parameters).find(pluginPath => pluginPath.match(reg));
         return _PluginManager_parameters.call(this, pluginPath);
     }
